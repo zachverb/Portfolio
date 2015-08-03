0ac05cc2c16c947b6c2e63dd200f1956198b42c1
zachverb <zachylax@gmail.com>
2015-08-02 22:37:54 -0700

    Updated necessary packages, created new webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path');
+var merge = require('webpack-merge');
+var fs = require('fs');
+var webpack = require('webpack');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var isDev = TARGET === 'dev';
+
+var jsxLoader = isDev ? ['react-hot', 'babel?stage=1'] : ['babel?stage=1'];
+
+var common = {
+  entry: path.resolve(ROOT_PATH, 'index.js'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'actions': path.resolve(ROOT_PATH, 'client', 'actions'),
+      'components': path.resolve(ROOT_PATH, 'client', 'components'),
+      'libs': path.resolve(ROOT_PATH, 'client', 'libs'),
+      'stores': path.resolve(ROOT_PATH, 'client', 'stores')
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  debug: false,
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: jsxLoader
+    },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      }]
+  }
+}
+
+if(TARGET === 'build') {
+  common = merge(common, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
+}
+
+if(TARGET === 'dev') {
+  common = merge(common, {
+    devtool: "#inline-source-map"
+  });
+}
+
+module.exports = common;
