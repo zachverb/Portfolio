66b3ccf2f98e0a55251f1cb76a9b228d7cf9febe
zachverb <zachylax@gmail.com>
2015-08-10 21:24:38 -0700

    Cleanup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + 'public/index.html'));
 })
 
 app.listen(port);
